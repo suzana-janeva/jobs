2f65a10bef9cc1b7800011e3103a08d38439d312
Suzana Janeva <suzana@codeart.mk>
2024-10-23 13:19:34 +0200

    added swagger docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,22 @@
 namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @OA\Info(
+ *    title="Gigs API",
+ *    version="1.0.0",
+ *    description="API documentation for Gigs API."
+ * ),
+ * 
+ *  * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      in="header",
+ *      name="bearerAuth",
+ *      type="http",
+ *      scheme="bearer",
+ *      bearerFormat="JWT",
+ * ),
+ */
 abstract class Controller
 {
     use AuthorizesRequests;
